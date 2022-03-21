4d4684b447494d7e567e3516ccaeb67ca4d06112
Nayeon Shin <nayeon.codes@gmail.com>
2022-03-21 14:28:57 -0400

    feat(`App.js`): Fetch and display data from YTS API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setIsLoading((current) => !current);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img alt="Cover" src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
