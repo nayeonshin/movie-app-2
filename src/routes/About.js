392b62d89cd76eecc765ff5ad646e6aace094780
Nayeon Shin <nayeon.codes@gmail.com>
2022-03-28 03:57:45 -0400

    feat(css): add styles to About page's card

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,9 @@
+import styles from "./About.module.css";
+
 function About() {
   return (
-    <div>
-      <p>
+    <div className={styles.card}>
+      <p className={styles.card__introduction}>
         Movie App is a simple function-based React app that lists movies from
         YTS.
       </p>
