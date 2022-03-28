461e75534aae402f680687706361fb3f8ebce421
Nayeon Shin <nayeon.codes@gmail.com>
2022-03-28 02:43:16 -0400

    feat(`Movie.js`): enclose `div` with `Link`

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,23 +5,23 @@ import styles from "./Movie.module.css";
 
 function Movie({ coverImage, genres, id, summary, title, year }) {
   return (
-    <div className={styles.movie}>
-      <img
-        alt={`${title}'s Cover`}
-        className={styles.movie__img}
-        src={coverImage}
-      />
-      <h2 className={styles.movie__title}>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <h3 className={styles.movie__year}>{year}</h3>
-      <p>{summary.length > 250 ? `${summary.slice(0, 250)}...` : summary}</p>
-      <ul className={styles.movie__genres}>
-        {genres?.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
-    </div>
+    <Link className={styles.link} to={`/movie/${id}`}>
+      <div className={styles.movie}>
+        <img
+          alt={`${title}'s Cover`}
+          className={styles.movie__img}
+          src={coverImage}
+        />
+        <h2 className={styles.movie__title}>{title}</h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <ul className={styles.movie__genres}>
+          {genres?.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+        <p>{summary.length > 250 ? `${summary.slice(0, 250)}...` : summary}</p>
+      </div>
+    </Link>
   );
 }
 
